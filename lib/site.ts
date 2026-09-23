f5245a5f3a31694dce76b734d6e448459fbc756d
John Fairfax-Ball <jfairfaxball@gmail.com>
2026-09-23 21:53:30 +0100

    fix: pass site URL environment explicitly

--- lib/site.ts
+++ lib/site.ts
@@ -36,5 +36,9 @@ export function resolveSiteUrl(env: SiteUrlEnvironment) {
 }
 
 export function getSiteUrl() {
-  return resolveSiteUrl(process.env);
+  return resolveSiteUrl({
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+    VERCEL_PROJECT_PRODUCTION_URL: process.env.VERCEL_PROJECT_PRODUCTION_URL,
+    VERCEL_URL: process.env.VERCEL_URL,
+  });
 }
